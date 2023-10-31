4ece556479683b91bc8216f8aa0df42cefba6736
Vygantas Vilius <biesaz@gmail.com>
2023-10-31 22:36:06 +0200

    exercises

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# Lesson 14: Logging ######################################
+# There are five ways we can display a log message in python with setting log levels: DEBUG, INFO, WARNING, ERROR, and CRITICAL.
+
+# The Five Levels of Logging (The severity level)
+# 10 DEBUG:          It is used for diagnosing the problem. It gives a piece of detailed information about the problem.
+# 20 INFO:           It gives the confirmation message of the successful execution of the program.
+# 30 WARNING:        The message is for when an unexpected situation occurs.
+# 40 ERROR:          It is due to a more serious problem than a warning. 
+#                    It can be due to some inbuilt error Like syntax or logical error.
+# 50 CRITICAL:       It occurs when the program execution stops and it can not run itself anymore.
+
 # import logging
 
 # logging.debug('This is a debug message')
@@ -5,11 +16,17 @@
 # logging.warning('This is a warning message')
 # logging.error('This is an error message')
 # logging.critical('This is a critical message')
-
 # -------------------OUTPUT---------------------
 # WARNING:root:This is a warning message
 # ERROR:root:This is an error message
 # CRITICAL:root:This is a critical message
+
+# basicConfig #########################################################
+# level:      To change the root logger to a specified severity level.
+# filename:   Filename where the logs going to be stored.
+# filemode:   If a filename is given then this specifies the file mode in which the file will open. default is append (a )
+# format:     This is the format of the log message.
+# datefmt:    It specified the date and time format.
 
 # import logging
 # logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
@@ -19,7 +36,27 @@
 # 12/05/2021 20:46:41 - root - DEBUG - This is Debug Message
 # 12/05/2021 20:46:41 - root - INFO - This is an info message
 
-import logging
-logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-name = input("Enter Your Name:\n")
-logging.info(f"{name} has logged in successfully !!")
+# Logging to file ##########################################################################################
+# Similarly, for logging the result** in a file** you can add filename and file mode to basicConfig:
+# logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='w')
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+# name = input("Enter Your Name:\n")
+# logging.info(f"{name} has logged in successfully !!")
+
+# Logging Exception ##################################
+# import logging
+# a = 10
+# b = 0
+# try:
+#   c = a / b
+# except Exception as e:
+#   logging.error("Exception Occurred", exc_info=True)  ## At default it is True
+  
+#  ----------------------------------CONSOLE OUTPUT------------------------
+# ERROR:root:Exception Occurred        ## If exc_info=False then only this message will print
+# Traceback (most recent call last):
+#   File "C:\Users\minde\Desktop\ds\python\advance_concepts\logging_code.py", line 5, in <module>
+#     c = a / b
+# ZeroDivisionError: division by zero
--- tasks.py
+++ tasks.py
@@ -2,30 +2,49 @@
 # Create a simple program that would log all inputs from the terminal. 
 # Configs must show all additional data (time, date, level etc.)
 
-import logging
-logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+# import logging
 
-list_of_ten_integers = []
-print("Enter 10 numbers: ")
-for numbers in range(10):
-    entered_numbers = int(input())
-    list_of_ten_integers.append(entered_numbers)
-sum_of_ten_integers = sum(list_of_ten_integers)
-average_of_ten_integers = sum_of_ten_integers / len(list_of_ten_integers)
-print("The sum of the entered 10 numbers is:", sum_of_ten_integers)
-print("The average of the entered 10 numbers is:", average_of_ten_integers)
+# logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='w')
 
-# logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+# list_of_ten_integers = []
+# print("Enter 10 numbers: ")
+# for numbers in range(10):
+#     entered_numbers = int(input())
+#     list_of_ten_integers.append(entered_numbers)
+# sum_of_ten_integers = sum(list_of_ten_integers)
+# average_of_ten_integers = sum_of_ten_integers / len(list_of_ten_integers)
+# print("The sum of the entered 10 numbers is:", sum_of_ten_integers)
+# print("The average of the entered 10 numbers is:", average_of_ten_integers)
+
+# logging.info(f"Entered numbers is {list_of_ten_integers}, their sum is {sum_of_ten_integers} and average is {average_of_ten_integers}. All logged successfully !!")
+
+# 2
+# Write a function that moves all elements of one type to the end of the list:
+# move_to_end([1, 3, 2, 4, 4, 1], 1) ➞ [3, 2, 4, 4, 1, 1]
+# Move all the 1s to the end of the array.
+# Log out inputs and results in a file.
 
 # import logging
 
-# logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-# name = input("Enter Your Name:\n")
-logging.info(f"{sum_of_ten_integers} has logged {list_of_ten_integers} successfully !!{average_of_ten_integers}")
+# logging.basicConfig(level=logging.DEBUG,filename='data.log', filemode='w')
 
+# def move_to_end(list, element):
+#     # Filter the elements to move to the end
+#     elements_to_move = [x for x in list if x == element]
+    
+#     # Remove the elements from the original list
+#     list = [x for x in list if x != element]
+    
+#     # Append the filtered elements to the end of the list
+#     list.extend(elements_to_move)
+    
+#     return list
 
-# 4
-# Create a program that takes 4 data types/structures: strings, numbers, list, dict. 
-# Iterate 10 times with inputs and log what data type/structure and how many times was entered. 
-# Handle all possible errors and log it.
+# # Test the function with your example
+# input_list = [1, 3, 2, 4, 4, 1]
+# element_to_move = 1
+# result = move_to_end(input_list, element_to_move)
+# print(result)
+
+# logging.info(f" Entered list is {input_list}, and moved list is {result}. All logged successfully !!")
 
